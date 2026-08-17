017177e268e28b063089b5c601a6d232a365959d
jameszhangcn2 <128544511+jameszhangcn2@users.noreply.github.com>
2026-08-17 07:33:57 +0200

    Add files via upload

--- AutoTest/tests/testHmi.py
+++ AutoTest/tests/testHmi.py
@@ -1,11 +1,15 @@
+import pytest
 import time
 from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent
 TEMPLATE_IMG = str(BASE_DIR / ".." / "CANoe" / "demo.png") # HMI参考模板图
 class TestHMI:
-    def test_hmi(self, cam_recorder, cam_picture, canoe_api):
+    @pytest.mark.parametrize("loop_index", list(range(10)))
+    def test_hmi(self, cam_recorder, cam_picture, canoe_api, kl15, loop_index):
+        print(f"第 {loop_index+1} 轮执行")
         canoeApi = canoe_api
         assert (canoeApi != None)
+        kl15.kl15on()
         
         keyonState = canoeApi.get_sys_var("Sysv_IGWorkCondition", "Sysv_IGWorkCondition")
         
@@ -18,7 +22,7 @@ class TestHMI:
      
         # 1. 设置总线信号 VehicleSpeed=80
         # set_signal(canoe, "VehicleSpeed", "DemoDB", 80)
-        time.sleep(20)  # 等待HMI界面刷新
+        time.sleep(30)  # 等待HMI界面刷新
         # 2. OpenCV摄像头视觉比对
         ssim_score = cam_picture.calc_ssim_camera_vs_template(TEMPLATE_IMG)
         print(f"SSIM相似度 = {ssim_score}")
@@ -28,8 +32,10 @@ class TestHMI:
         
         # keyoff
         canoeApi.set_sys_var("Sysv_IGWorkCondition", "Sysv_IGWorkCondition", 1)
-        time.sleep(1) 
+        kl15.kl15off()
+        time.sleep(3) 
      
         # 4. pytest断言：相似度大于0.92才算PASS
         assert ssim_score >= 0.92, f"HMI界面校验失败，SSIM={ssim_score}"
+        time.sleep(5) 
         pass
