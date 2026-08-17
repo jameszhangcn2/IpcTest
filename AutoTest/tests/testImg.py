40e50ecd871ca5d5d2ce74b0b6072c0311c2d619
jameszhangcn2 <128544511+jameszhangcn2@users.noreply.github.com>
2026-08-17 16:53:12 +0200

    Add files via upload

---
+++ AutoTest/tests/testImg.py
@@ -0,0 +1,20 @@
+import pytest
+from utils.image_check import template_match_exist
+from pathlib import Path
+BASE_DIR = Path(__file__).resolve().parent
+TEMPLATE_IMG = str(BASE_DIR / ".." / "testTemplate" / "normal.png") # HMI参考模板图
+#ODOMETER_IMG = str(BASE_DIR / ".." / "testTemplate" / "odometerFake.png") # HMI局域匹配模板图
+ODOMETER_IMG = str(BASE_DIR / ".." / "testTemplate" / "odometer.png") # HMI局域匹配模板图
+class TestIMG:
+    @pytest.mark.parametrize("loop_index", list(range(1))) 
+    def test_check_icon_exists(self, case_logger, loop_index):
+        print(f"第 {loop_index+1} 轮执行")
+        # 截图文件（执行测试前先完成截图动作）
+        screen_img = TEMPLATE_IMG
+        target_icon = ODOMETER_IMG
+
+        exists, score, pos = template_match_exist(screen_img, target_icon, threshold=0.8)
+        print(f"最大匹配分数: {score:.3f}")
+
+        # pytest断言：界面元素必须存在
+        assert exists is True, f"未找到目标图标，匹配分数仅 {score:.3f}"
