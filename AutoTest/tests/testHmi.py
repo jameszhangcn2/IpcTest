dbc494d2dce36a3d5a94a6ab04172e7289dba9a2
jameszhangcn2 <128544511+jameszhangcn2@users.noreply.github.com>
2026-08-17 15:39:16 +0200

    Add files via upload

--- AutoTest/tests/testHmi.py
+++ AutoTest/tests/testHmi.py
@@ -2,7 +2,7 @@ import pytest
 import time
 from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent
-TEMPLATE_IMG = str(BASE_DIR / ".." / "CANoe" / "demo.png") # HMI参考模板图
+TEMPLATE_IMG = str(BASE_DIR / ".." / "testTemplate" / "normal.png") # HMI参考模板图
 class TestHMI:
     @pytest.mark.parametrize("loop_index", list(range(10)))
     def test_hmi(self, cam_recorder, cam_picture, canoe_api, kl15, loop_index):
@@ -11,31 +11,24 @@ class TestHMI:
         assert (canoeApi != None)
         kl15.kl15on()
         
-        keyonState = canoeApi.get_sys_var("Sysv_IGWorkCondition", "Sysv_IGWorkCondition")
+        measurement = canoe_api.app.Measurement
+ 
+        # 启动测量
+        if not measurement.Running:
+            measurement.Start()
+        time.sleep(30) # 等待HMI界面刷新
         
-        print("\n Sysv_IGWorkCondition ", keyonState)
-        
-        
-        canoeApi.set_sys_var("Sysv_IGWorkCondition", "Sysv_IGWorkCondition", 5)
-        time.sleep(1) 
-        print("\n Sysv_IGWorkCondition ", canoeApi.get_sys_var("Sysv_IGWorkCondition", "Sysv_IGWorkCondition"))
-     
-        # 1. 设置总线信号 VehicleSpeed=80
-        # set_signal(canoe, "VehicleSpeed", "DemoDB", 80)
-        time.sleep(30)  # 等待HMI界面刷新
         # 2. OpenCV摄像头视觉比对
         ssim_score = cam_picture.calc_ssim_camera_vs_template(TEMPLATE_IMG)
         print(f"SSIM相似度 = {ssim_score}")
-     
-        # 3. 将视觉结果回写给CANoe系统变量
-        #set_sys_var(canoe, "VisualTest", "SSIM_Result", ssim_score)
+        time.sleep(40)  # 等待CAN message end
         
-        # keyoff
-        canoeApi.set_sys_var("Sysv_IGWorkCondition", "Sysv_IGWorkCondition", 1)
+        #shut down KL15, stop the measurement
         kl15.kl15off()
-        time.sleep(3) 
+        if measurement.Running:
+            measurement.Stop()
+        time.sleep(20) 
      
         # 4. pytest断言：相似度大于0.92才算PASS
-        assert ssim_score >= 0.92, f"HMI界面校验失败，SSIM={ssim_score}"
-        time.sleep(5) 
+        assert ssim_score >= 0.92, f"HMI界面校验失败，SSIM={ssim_score}" 
         pass
